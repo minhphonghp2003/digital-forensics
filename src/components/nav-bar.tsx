02b4e192f830d0167f2d9f8f7ca2f4eef52fe291
minhphonghp2003 <minhphonghp2003@gmail.com>
2025-04-21 22:33:20 +0700

    layout

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,11 +1,16 @@
 
 
 import { Input } from "@/components/ui/input"
+import Link from "next/link"
 function NavBar() {
     return (
         <nav>
             <div className="fixed top-0  w-full z-40 flex justify-between items-center p-4 bg-white shadow-md ">
-                <h1 className="text-2xl font-bold">Phong Digital forensics</h1>
+                <h1 className="text-2xl font-bold">
+                    <Link href="/" className="flex items-center gap-2 text-orange-500">
+                        Phong Digital forensics
+                    </Link>
+                </h1>
                 <ul className="flex space-x-4 items-center">
                     <li>
                         <Input placeholder="Search..." />
